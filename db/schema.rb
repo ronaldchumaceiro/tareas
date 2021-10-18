52039c716f376c0974cbad89a1e73f3ff535e395
Zolution <>
2021-10-18 11:31:12 +0000

    create modelo tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_10_18_112158) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "description"
+    t.boolean "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
